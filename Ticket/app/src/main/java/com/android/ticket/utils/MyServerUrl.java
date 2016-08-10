d7865c8bcf1ff0dbadbb85589dde084429d880e8
NLTE <HGQ577068802@163.com>
2016-08-10 23:24:51 +0800

    添加各个作业相对应的项目文件，清空多余的文件

--- Ticket/app/src/main/java/com/android/ticket/utils/MyServerUrl.java
+++
@@ -1,6 +0,0 @@
-package com.android.ticket.utils;
-
-public class MyServerUrl {
-	public static String url = "http://172.18.150.84:8080/TicketServer1/";
-//	public static String url = "http://192.168.1.42:8080/TicketServer1/";
-}
--- Ticket/app/src/main/java/com/android/ticket/utils/NetworkRequest.java
+++
@@ -1,7 +0,0 @@
-package com.android.ticket.utils;
-
-/**
- * Created by Administrator on 2016/7/17.
- */
-public class NetworkRequest {
-}
